f72c927b5f353228c3d5229a4a177fcabe492400
andreasfloros <77194848+andreasfloros@users.noreply.github.com>
2022-10-27 11:35:45 +0100

    pbkdf2

--- crypto.js
+++ crypto.js
@@ -1,20 +1,36 @@
 const CryptoJS = require("crypto-js");
 const fs = require("fs");
 
+const generateCfg = (password, salt) => {
+    if (salt === null) {
+        salt = CryptoJS.lib.WordArray.random(16).toString();
+    }
+    console.log(salt)
+    console.log(salt.length)
+    let out = CryptoJS.PBKDF2(password, salt, {keySize: 12, iterations: 1000});
+    out.clamp();
+    return {
+        key: CryptoJS.lib.WordArray.create(out.words.slice(0, 8)),
+        iv: CryptoJS.lib.WordArray.create(out.words.slice(8)),
+        salt: salt
+    };
+}
+
 module.exports = (password, filePath) => {
     const auth = '12D4500890k2+456';
-    const dataFile = fs.readFileSync(filePath);
-    const dataBase64 = dataFile.toString('base64');
+    const data = fs.readFileSync(filePath).toString('base64');
     let result = null;
-    if (dataBase64.slice(0, auth.length) === auth) {
-        result = CryptoJS.AES.decrypt(dataBase64.slice(auth.length), password, {mode: CryptoJS.mode.OFB}).toString(CryptoJS.enc.Utf8);
+    if (data.slice(0, auth.length) === auth) {
+        const {key, iv, _} = generateCfg(password, data.slice(auth.length, auth.length + 32));
+        result = CryptoJS.AES.decrypt(data.slice(auth.length + 32), key, {iv: iv, mode: CryptoJS.mode.OFB}).toString(CryptoJS.enc.Utf8);
         if (result.slice(0, auth.length) !== auth) {
             return false;
         }
         result = result.slice(auth.length);
     }
     else {
-        result = auth + CryptoJS.AES.encrypt(auth + dataBase64, password, {mode: CryptoJS.mode.OFB}).toString();
+        const {key, iv, salt} = generateCfg(password, null);
+        result = auth + salt + CryptoJS.AES.encrypt(auth + data, key, {iv: iv, mode: CryptoJS.mode.OFB}).toString();
     }
     const buffer = Buffer.from(result, 'base64');
     fs.writeFileSync(filePath, buffer);
